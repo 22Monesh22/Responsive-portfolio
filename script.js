84136ad35df77dda39fbcfbb7a5398c4724605b0
Sowmiya G <145428278+23sowmiya23@users.noreply.github.com>
2023-11-01 11:45:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,13 @@
-// window.addEventListener('contextmenu', function (e) {
-//   e.preventDefault();
-// });
+const skills = document.querySelectorAll(".skill");
 
-// // Disable F12 key and Ctrl+Shift+I (Cmd+Option+I on Mac) to open DevTools
-// window.addEventListener('keydown', function (e) {
-//   if ((e.key === 'F12' && e.ctrlKey === true) || (e.key === 'I' && e.ctrlKey === true && e.shiftKey === true)) {
-//       e.preventDefault();
-//   }
-// });
+window.addEventListener("scroll", () => {
+    skills.forEach((skill) => {
+        const skillPosition = skill.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
 
-// Sticky Navigation Menu JS Code
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
-window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
-    nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
-  }else{
-    nav.classList.remove("sticky");
-    scrollBtn.style.display = "none";
-  }
-
-}
-
-// Side NavIgation Menu JS Code
-let body = document.querySelector("body");
-let navBar = document.querySelector(".navbar");
-let menuBtn = document.querySelector(".menu-btn");
-let cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = function(){
-  navBar.classList.add("active");
-  menuBtn.style.opacity = "0";
-  menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
-  scrollBtn.style.pointerEvents = "none";
-}
-cancelBtn.onclick = function(){
-  navBar.classList.remove("active");
-  menuBtn.style.opacity = "1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
-}
-
-// Side Navigation Bar Close While We Click On Navigation Links
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
-    navBar.classList.remove("active");
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-  });
-}
+        if (skillPosition < windowHeight) {
+            const progressBar = skill.querySelector("progress");
+            progressBar.style.width = progressBar.getAttribute("value") + "%";
+        }
+    });
+});
